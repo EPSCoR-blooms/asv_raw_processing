d0faf2cca6fc14250aa0b06dd507dc0bf20b0319
B Steele <steeleb@caryinstitute.org>
2022-01-31 15:04:23 -0700

    completed manual processing script for sunapee 2021-07-22

--- x.manual_process.R
+++ x.manual_process.R
@@ -8,6 +8,7 @@
 pacman::p_unload(pacman::p_loaded(), character.only = TRUE)
 
 #load libraries
+library(data.table)
 library(tidyverse)
 
 # determine OS for automatic filepath determination
@@ -29,6 +30,7 @@ comp_dir = paste0(path_pat, 'project_data/ASV_data/compiled/')
 lake_dir = paste0(path_pat, 'project_data/ASV_data/manual_process/')
 meta_dir = paste0(path_pat, 'project_data/ASV_data/metadata/')
 path_dir = paste0(path_pat, 'project_data/ASV_data/raw_path_data/')
+proc_dir = paste0(path_pat, 'project_data/ASV_data/analysis_ready/')
 
 ## Read in previously-created files required for processing ----
 metadata <- read.csv(file.path(comp_dir, 'compiled_ASV_deployment_general_info.csv'))
@@ -159,7 +161,6 @@ wp_colnames = c('waypoint_seq', 'wp_seq', 'wp_coord_frame', 'wp_command',
                 'wp_param1', 'wp_param2', 'wp_param3', 'wp_param4', 'wp_param_lat', 
                 'wp_param_long', 'wp_param_alt', 'wp_autocontinue')
 
-
 for(k in 1:length(wp_list)) {
   df = read.delim(file.path(path_dir, wp_list[k]),
                   sep = '\t',
@@ -183,7 +184,9 @@ all_wp <- all_wp %>%
 rosmav_m1 <- left_join(rosmav_m1, all_wp)
 rosmav_m2 <- left_join(rosmav_m2, all_wp)
 
-### left off here-----
+all_rosmav <- full_join(rosmav_m1, rosmav_m2)
+
+# add CV
 
 #join rosmav and waypoints files
 rosmav_wp <- full_join(all_rosmav, all_wp) %>% 
@@ -195,5 +198,91 @@ rosmav_wp <- rosmav_wp %>%
          timestamp_header_sec, header_seq, waypoint_seq, wp_command, wp_param1,
          rosmav_filename, path_filename, ASV_processed_filename)
 
-# save rosmav_wp file
-write.csv(rosmav_wp, file.path(inter_dir, 'rosmav_waypoints_allmissions.csv'), row.names = F)
+rosmav_wp <- rosmav_wp%>% 
+  rename(timestamp_gps_sec = timestamp_header_sec) 
+
+#subset asv data
+asv <- asv_data %>% 
+  mutate(timestamp_gps_sec_integer = as.integer(timestamp_gps_sec)) 
+#subset wp data
+wp <- rosmav_wp %>% 
+  rename(timestamp_gps_sec_integer = timestamp_gps_sec) %>% 
+  mutate(timestamp_gps_sec_integer = as.integer(timestamp_gps_sec_integer))
+#join asv and wp data
+asv_wp <- left_join(asv,wp) %>% 
+  arrange(timestamp_gps_sec) %>% 
+  select(-timestamp_gps_sec_integer)
+
+#remove data before first waypoint of mission 
+wpsq_min = min(asv_wp$waypoint_seq, na.rm = T)[1]
+ix = which(asv_wp$waypoint_seq == wpsq_min)[1]
+
+start_wp = asv_wp$timestamp_gps_sec[ix]
+asv_wp_trunc <- asv_wp %>% 
+  filter(timestamp_gps_sec >= start_wp)
+
+# find out which is later, last waypoint or within 50m of deployment, or truncation of last minute
+
+# last waypoint method:
+last_headerseq = max(asv_wp_trunc$header_seq, na.rm = T)
+zx = which(asv_wp_trunc$header_seq == last_headerseq)
+lastseq_wp = asv_wp_trunc$timestamp_gps_sec[zx]
+
+# remove last 5 minutes:
+last_timestamp = max(asv_wp_trunc$timestamp_gps_sec, na.rm = T)-(5*60)
+df = asv_wp_trunc %>% 
+  filter(timestamp_gps_sec < last_timestamp)
+mx = last(df$timestamp_gps_sec)
+lastseq_time = asv_wp_trunc$timestamp_gps_sec[asv_wp_trunc$timestamp_gps_sec == mx]
+
+if(lastseq_wp > lastseq_time) {
+  message('last waypoint timestamp occurs after 5 minute truncation, truncating at last waypoint')
+  asv_wp_trunc <- asv_wp_trunc %>% 
+    filter(timestamp_gps_sec<lastseq_wp)
+  asv_wp_trunc$eor_trunc_method = 'last waypoint'
+  asv_wp_trunc$eor_flag = ''
+} else {
+  message('5 minute truncation occurs after last waypoint timestamp, truncating at 5 minute prior to last record')
+  last_ts = asv_wp_trunc$timestamp_gps_sec[asv_wp_trunc$timestamp_gps_sec == mx]
+  asv_wp_trunc <- asv_wp_trunc %>% 
+    filter(timestamp_gps_sec <= last_ts)
+  asv_wp_trunc$eor_trunc_method = '5 minutes'
+  asv_wp_trunc <- asv_wp_trunc %>% 
+    mutate(eor_flag = case_when(timestamp_gps_sec>=lastseq_wp ~ 'data collected after last programmed waypoint, use with caution',
+                                TRUE ~ ''))
+}
+
+#add flags for loiter
+lx = which(wp$wp_command == 19)
+loiter_end = wp$timestamp_gps_sec[lx]
+loiter_length = wp$wp_param1[lx]
+loiter_start = loiter_end-loiter_length
+
+#initialize loiter flag column
+asv_wp_trunc$loiter_flag = ''
+
+for(l in 1:length(loiter_start)){
+  asv_wp_trunc <- asv_wp_trunc %>% 
+    mutate(loiter_flag = case_when(timestamp_gps_sec >= loiter_start[l] & timestamp_gps_sec <= loiter_end[l] ~ 'l',
+                                   TRUE ~ loiter_flag))
+}
+
+#add speed flag for movement faster than 1mps
+asv_wp_trunc$velocity_flag = ''
+sx = which(asv_wp_trunc$velocity_gps_mps > 1)
+asv_wp_trunc$velocity_flag[sx] = 'gt1mps'
+
+#get lake name and deployment dates
+lake_folder = asv_wp_trunc$lake[1]
+deployment_date = asv_wp_trunc$date[1]
+deployment_inst = asv_wp_trunc$deployment_instance[1]
+
+#re-order columns
+col_names_asv <- colnames(asv_wp_trunc)
+
+asv_wp_trunc <- asv_wp_trunc %>% 
+  select(lake, year, date, lab, equipment, deployment_type, deployment_instance, eor_trunc_method, eor_flag, velocity_flag, loiter_flag, everything())
+
+#write file to processed folder
+fwrite(asv_wp_trunc, file.path(proc_dir, lake_folder, paste0(lake_folder, '_', deployment_date, '_', deployment_inst, '_asv_processed_v', Sys.Date(), '.csv')), row.names = F)
+message(paste0('file processed and saved as ',lake_folder, '_', deployment_date,'_', deployment_inst,  '_asv_processed_v', Sys.Date(), '.csv'))
